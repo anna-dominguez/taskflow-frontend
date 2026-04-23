4bb97dfef7fd624bf3821826ba42d28924f87ef5
anna-dominguez <anna.dominguez.anna@gmail.com>
2026-04-23 17:29:59 +0200

    move sentry test button

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
 					<button
 						type="button"
 						className="mt-2 bg-red-600 text-white px-4 py-2 rounded-lg text-sm font-mono shadow-lg"
-						onClick={() => Sentry.captureException(new Error('Erreur de test Sentry (React)'))}
+						onClick={() => {throw new Error('Test Sentry')}}
 					>
 						Test Sentry
 					</button>
